09d3e648135d397a474dcd8756400debe025ad3f
superbenji711 <benjilabonte123@gmail.com>
2021-01-30 13:53:34 -0500

    nav bar

---
+++ client/src/navigations/navbar.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './NavBar.css';
+import logo from '../../assets/logo.jpg';
+import { Link } from 'react-router-dom';
+import {
+    Button,
+    Menu,
+    Dropdown, 
+    Checkbox
+  } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+
+const NavBar = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar;
+
