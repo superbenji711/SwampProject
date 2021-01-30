f79c631bdc40fae6c7e60eeb3bc96bc2c4e35f85
superbenji711 <benjilabonte123@gmail.com>
2021-01-30 13:57:38 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import NavBar from './navigations/navbar'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
           Yoooo
       </header>
     </div>
--- client/src/navigations/navbar.js
+++ client/src/navigations/navbar.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import './NavBar.css';
-import logo from '../../assets/logo.jpg';
+//import logo from '../../assets/logo.jpg';
 import { Link } from 'react-router-dom';
 import {
     Button,
     Menu,
-    Dropdown, 
+    Dropdown,
     Checkbox
-  } from 'semantic-ui-react'
+} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
 const NavBar = (props) => {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="nav-bars">
+                <Dropdown
+                    icon='bars'
+                    floating
+                    className='DropIcon'
+                >
+                </Dropdown>
+            </div>
         </div>
+
     )
 }
 
