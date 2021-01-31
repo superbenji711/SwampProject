fe08faa6fbe66d5d570b3e9bf710869c97e650c4
superbenji711 <benjilabonte123@gmail.com>
2021-01-31 01:35:45 -0500

    reciepes

--- client/src/components/Recipes/recipes_components/Results.js
+++ client/src/components/Recipes/recipes_components/Results.js
@@ -1,20 +1,99 @@
 import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Card, Icon } from 'semantic-ui-react'
+
 import './Results.css'
 
 const Results = () => {
+
+    const list = [
+        {
+            "name": "Leia Burger",
+            "ingredients": ["cheese", "meat"],
+            "id": 1
+        },
+        {
+            "name": "Luke Burger",
+            "ingredients": ["tomato", "meat", "lettuce"],
+            "id": 2
+        },
+        {
+            "name": "Ham Solo Burger",
+            "ingredients": ["bacon", "cheese", "meat"],
+            "id": 3
+        },
+        {
+            "name": "Double Ham Solo",
+            "ingredients": ["bacon", "cheese", "meat", "cheese", "meat"],
+            "id": 4
+        },
+        {
+            "name": "All-green Yoda Burger",
+            "ingredients": ["cheese", "tomato", "cucumber", "lettuce"],
+            "id": 5
+        },
+        {
+            "name": "The Chewbacca",
+            "ingredients": ["tomato", "cucumber", "cheese", "meat", "meat", "meat"],
+            "id": 6
+        },
+        {
+            "name": "Darth Vader Burger",
+            "ingredients": ["tomato", "cucumber", "bacon", "cheese", "meat", "lettuce"],
+            "id": 7
+        },
+        {
+            "name": "The Emperor",
+            "ingredients": [
+                "bacon",
+                "cheese",
+                "meat",
+                "bacon",
+                "cheese",
+                "meat",
+                "bacon",
+                "cheese",
+                "meat"
+            ],
+            "id": 8
+        }
+    ]
     return (
-        <div className="recipe">
-           <img src="https://www.besthealthmag.ca/wp-content/uploads/2019/07/junk-food-1.gif" alt="food" width="150px" height="100px"/>
-           <p>Description</p>
-           <ul className="recipeList">
-               <li>Ingredient</li>
-               <li>Ingredient</li>
-               <li>Ingredient</li>
-               <li>Ingredient</li>
-           </ul>
-           <p>Calories</p>
-        </div>
+        <Grid centered={true} divided columns={'equal'} >
+
+
+            {list.map((recipe, i) => (
+                <Grid.Row >
+                    <Grid.Column columns={2}>
+                        <Card.Group>
+                            <Card centered key={i}>
+
+                                <Card.Content header={recipe.name} />
+                                <Image  src={"https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80"}
+                                size='medium'  centered ></Image>
+                                <div className="card">
+                                   
+                                    <Card.Description textAlign={true} content={recipe.ingredients.map((ingredient, i) => (
+                                        <div>{i+1 + ". "}{ingredient}</div>
+                                    ))} />
+
+                                </div>
+
+
+
+                                {/* <Card.Content key={i}>{recipe.ingredients.map((Ingredient)=>(
+                                {Ingredient}
+                            ))}</Card.Content> */}
+
+
+                            </Card>
+                        </Card.Group>
+                    </Grid.Column>
+                </Grid.Row>
+            ))}
+
+
+
+        </Grid>
     )
 }
 
